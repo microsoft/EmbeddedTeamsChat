6a48f9cb09767be4a1d6b508349678b8789ec453
Richard diZerega <ridize@microsoft.com>
2023-06-13 08:36:38 -0500

    Updated for decrypted content sent with new messages

--- wwwroot/src/NotificationClients/GraphNotificationClient.ts
+++ wwwroot/src/NotificationClients/GraphNotificationClient.ts
@@ -118,17 +118,22 @@ export class GraphNotificationClient implements INotificationClient {
         });
 
         // message received
-        this.connection.on(this.ReturnMethods.NewMessage, async (notification) => {
+        this.connection.on(this.ReturnMethods.NewMessage, async (notification, decryptedContent) => {
 
-            // decrypt the content
-            const response = await fetch(ConfigUtil.GnbEndpoint + "/api/GetChatMessageFromNotification", {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${gnbToken.accessToken}`
-                },
-                body: JSON.stringify(notification.encryptedContent)
-            });
-            var decryptedResourceData:any = await response.json();
+            // make descryption backwards compatible
+            var decryptedResourceData:any = decryptedContent;
+
+            if (!decryptedResourceData) {
+                // decrypt the content
+                const response = await fetch(ConfigUtil.GnbEndpoint + "/api/GetChatMessageFromNotification", {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${gnbToken.accessToken}`
+                    },
+                    body: JSON.stringify(notification.encryptedContent)
+                });
+                decryptedResourceData = await response.json();
+            }
 
             // get the operation type
             const op:Operation = (notification.changeType == "created") ? Operation.Created 
